d25f598e36f2cee23f75e732ddd801806f974af3
Jerry <Jvictor940@gmail.com>
2022-09-30 17:31:16 -0400

    for loop for hex pg to generate diff pattern or #s

--- public/main.js
+++ public/main.js
@@ -1,6 +1,6 @@
 // 'use strict';
 
-const colors = ["green", "red", "rgba(133,122,200)", "#f15025"]
+const colors = ["green", "red", "purple", "red-orange"]
 const btn = document.getElementById('btn');
 const color = document.querySelector(".color");
 btn.addEventListener("click", () => {
