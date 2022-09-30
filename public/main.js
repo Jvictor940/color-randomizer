ed57295ac6c5368f554aa8db43754a314039a33a
Jerry <Jvictor940@gmail.com>
2022-09-30 17:01:39 -0400

    eventListener background color change after click

--- public/main.js
+++ public/main.js
@@ -1,3 +1,13 @@
 // 'use strict';
 
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"]
+const btn = document.getElementById('btn');
+const color = document.querySelector(".color");
+btn.addEventListener("click", () => {
+    // get random number between 0 and 3
+    const randomNumber = 2;
+    document.body.style.backgroundColor = colors[randomNumber];
+    color.textContent = colors[randomNumber]; 
+})
+
+// function getRandomNumber() {}
